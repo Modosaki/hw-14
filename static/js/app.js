aea34314ae0083f33ce2f16c4aba7d52548f24d6
Karim <34213302+Modosaki@users.noreply.github.com>
2018-11-29 13:20:11 -0600

    Add files via upload

--- static/js/app.js
+++ static/js/app.js
@@ -48,6 +48,7 @@ submit.on("click", function() {
 
   // Select the input element and get the raw HTML node
   var inputElement = d3.select("#datetime");
+   
 
   // Get the value property of the input element
   var inputValue = inputElement.property("value");
@@ -84,23 +85,4 @@ submit.on("click", function() {
 });
 
 
-//   BONUS: Calculate summary statistics for the age field of the filtered data
-
-  // First, create an array with just the age values
-//  var ages = filteredData.map(person => person.age);
-//
-//  // Next, use math.js to calculate the mean, median, mode, var, and std of the ages
-//  var mean = math.mean(ages);
-//  var median = math.median(ages);
-//  var mode = math.mode(ages);
-//  var variance = math.var(ages);
-//  var standardDeviation = math.std(ages);
-//
-//  // Finally, add the summary stats to the `ul` tag
-//  d3.select(".summary")
-//    .append("li").text(`Mean: ${mean}`)
-//    .append("li").text(`Median: ${median}`)
-//    .append("li").text(`Mode: ${mode}`)
-//    .append("li").text(`Variance: ${variance}`)
-//    .append("li").text(`Standard Deviation: ${standardDeviation}`);
 });
